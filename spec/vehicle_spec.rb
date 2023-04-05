d91d57f31737b14790edcb9b57519a31a1e5c8a2
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-05 09:25:51 -0600

    Test: #initialize, #speeding?, #speed, #add_passenger, #num_adults

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+RSpec.describe Vehicle do
+  before(:each) do
+    @vehicle = Vehicle.new("2001", "Honda", "Civic")   
+    @charlie = Passenger.new({
+      "name" => "Charlie", 
+      "age" => 18
+    })  
+    @jude = Passenger.new({
+      "name" => "Jude", 
+      "age" => 20
+    })
+    @taylor = Passenger.new({
+      "name" => "Taylor", 
+      "age" => 12
+    })
+  end
+
+  describe '#initialize' do 
+    it 'exists' do 
+      expect(@vehicle).to be_a Vehicle
+    end
+
+    it 'has attributes' do 
+      expect(@vehicle.year).to eq('2001')
+      expect(@vehicle.make).to eq('Honday')
+      expect(@vehicle.year).to eq('Civic')
+    end
+  end
+
+  describe 'speed' do 
+    it '#speeding?' do 
+      expect(@vehicle.speeding?).to be false
+    end
+
+    it '#speed' do 
+      @vehicle.speed
+      expect(@vehicle.speeding?).to be true
+    end
+  end
+
+  describe 'passengers' do 
+    it 'starts without passengers' do 
+      expect(@vehicle.passengers).to eq([])
+    end
+
+    it '#add_passenger' do 
+      @vehicle.add_passenger(@charlie)
+      @vehicle.add_passenger(@jude)
+      @vehicle.add_passenger(@taylor)
+      expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
+    end
+  end
+
+  describe 'adulthood' do 
+    it '#num_adults' do 
+      @vehicle.add_passenger(@charlie)
+      @vehicle.add_passenger(@jude)
+      @vehicle.add_passenger(@taylor)
+      expect(@vehicle.num_adults).to eq(2)
+    end
+  end
+end
