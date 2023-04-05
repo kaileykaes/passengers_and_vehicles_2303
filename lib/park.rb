b4b3f99b354a71e9dd017ea9106bdcb3afd9437f
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-05 10:51:59 -0600

    format

--- lib/park.rb
+++ lib/park.rb
@@ -16,25 +16,25 @@ class Park
   end
 
   def passengers
-    @vehicles.map {|vehicle| vehicle.passengers}.flatten!
+    @vehicles.map{ |vehicle| vehicle.passengers }.flatten!
   end
 
   def revenue
-    @vehicles.map {|vehicle| @revenue += @admission_price * vehicle.num_adults}
+    @vehicles.map{ |vehicle| @revenue += @admission_price * vehicle.num_adults }
     @revenue
   end
 
   def all_attendees
-    passengers.map {|passenger| passenger.name}.sort
+    passengers.map{ |passenger| passenger.name }.sort
   end
 
   def minors 
-    children = passengers.find_all {|passenger| passenger.adult? != true}
-    children.map {|child| child.name}.sort
+    children = passengers.find_all{ |passenger| passenger.adult? != true }
+    children.map{ |child| child.name }.sort
   end
 
   def adults 
-    adults = passengers.find_all {|passenger| passenger.adult?}
-    adults.map{|adult| adult.name}.sort
+    adults = passengers.find_all{ |passenger| passenger.adult? }
+    adults.map{ |adult| adult.name }.sort
   end
 end
