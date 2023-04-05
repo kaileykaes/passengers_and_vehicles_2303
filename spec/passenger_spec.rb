d4a5043ae4bbfd4facaaa2961b9073008babb8fa
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-05 09:11:28 -0600

    Test: #adult?

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -17,4 +17,11 @@ RSpec.describe Passenger do
       expect(@charlie.age).to eq(18)
     end
   end
+
+  describe '#adult?' do 
+    it 'knows if adult' do 
+      expect(@charlie.adult?).to be true
+      expect(@taylor.adult?).to be false
+    end
+  end
 end
