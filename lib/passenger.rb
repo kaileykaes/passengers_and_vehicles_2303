f1736c8207a09de2af1114117b4bafc3797997cf
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-05 09:10:17 -0600

    Feat: initialize

---
+++ lib/passenger.rb
@@ -0,0 +1,10 @@
+class Passenger
+  attr_reader :name, 
+              :age
+  
+  def initialize(passenger_details)
+    @name = passenger_details['name']
+    @age = passenger_details['age']
+    @argument = passenger_details[:argument]
+  end
+end
