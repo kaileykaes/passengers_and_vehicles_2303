5b92c2b4b339450e1e44c8df83f4ee51a14683fa
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-05 10:17:41 -0600

    Feat: #vehicles

--- lib/park.rb
+++ lib/park.rb
@@ -1,9 +1,11 @@
 class Park
   attr_reader :name, 
-              :admission_price
+              :admission_price, 
+              :vehicles
   
   def initialize(name, admission_price)
     @name = name
     @admission_price = admission_price
+    @vehicles = []
   end
 end
